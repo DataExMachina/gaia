f2aacdb1a8ede2f0e4d2d71f5ba607d377ce8e9c
DataExMachina <binvignat.j@gmail.com>
2020-03-26 01:18:07 +0100

    Start writting base class

--- gaia/gaia.py
+++ gaia/gaia.py
@@ -1,1 +1,35 @@
 """Main module."""
+
+from abc import ABCMeta, abstractmethod
+
+from sklearn.base import BaseEstimator
+from sklearn.utils import check_array, check_X_y
+from sklearn.utils.validation import check_is_fitted
+
+class SpatialRegressor(BaseEstimator, metaclass=ABCMeta):
+    """Base class for Linear Models"""
+
+    @abstractmethod
+    def fit(self, X, y):
+        """Fit model."""
+
+    def _decision_function(self, X):
+        check_is_fitted(self)
+
+        X = check_array(X, accept_sparse=['csr', 'csc', 'coo'])
+        return safe_sparse_dot(X, self.coef_.T,
+                               dense_output=True) + self.intercept_
+
+    def predict(self, X):
+        """
+        Predict using the linear model.
+        Parameters
+        ----------
+        X : array_like or sparse matrix, shape (n_samples, n_features)
+            Samples.
+        Returns
+        -------
+        C : array, shape (n_samples,)
+            Returns predicted values.
+        """
+        return self._decision_function(X)
